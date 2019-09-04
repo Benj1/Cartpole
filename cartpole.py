17ea66ec2e15a7fb05c2f949140d639153f53315
Ben McDonnell <mcdonnell.benjamin@gmail.com>
2019-09-04 12:22:28 +0100

    Working version of tiling approximation

--- cartpole.py
+++ cartpole.py
@@ -12,36 +12,63 @@ class RandomAgent(object):
 class BetterAgent(object):
   def __init__(self, action_space):
     self.action_space = action_space
-    self.weights = np.array([20,20,20,20,20,20,20,20, 0])
+    self.weights = np.zeros((800, 8))
+    self.iht = tiles.IHT(800)
+    self.normalise = np.array([4,1,4,1,10])
+    self.training_rate = 0.1
+
   def act(self, state, reward):
-    a0 = np.dot(self.weights, np.append(state,0))
-    a1 = np.dot(self.weights, np.append(state,1))
-    # Action is weighted random choice  
-    a0prob = a0 / (a0 + a1)
-    a1prob = a1 / (a0 + a1)
-    action = np.random.choice([0,1], p=[a0prob, a1prob])
+    x0 = tiles.tiles(self.iht, 8, np.append(state, 0)*self.normalise)
+    x1 = tiles.tiles(self.iht, 8, np.append(state, 1)*self.normalise)
+    a0 = sum([self.weights[x0[i]][i] for i in range(8)])
+    a1 = sum([self.weights[x1[i]][i] for i in range(8)])
+    if np.random.rand() < 0.8:
+      if a0 > a1:
+        action = 0
+      else:
+        action = 1
+    else:
+      if a1 > a0:
+        action = 0
+      else:
+        action = 1
     return action
-  def update_weights(self):
-    pass
-    
+
+  def update_weights(self, reward, state, new_state, action, new_action):
+    x = tiles.tiles(self.iht, 8, np.append(state, action)*self.normalise)
+    new_x = tiles.tiles(self.iht, 8, np.append(new_state, new_action)*self.normalise)
+    q = sum([self.weights[x[i]][i] for i in range(8)])
+    new_q = sum([self.weights[new_x[i]][i] for i in range(8)])
+    for i in range(8):
+      self.weights[x[i]][i] += self.training_rate*(reward + new_q - q)
+  
+  def terminal_update(self, reward, state, action):
+    x = tiles.tiles(self.iht, 8, np.append(state, action)*self.normalise)
+    q = sum([self.weights[x[i]][i] for i in range(8)])
+    for i in range(8):
+      self.weights[x[i]][i] += self.training_rate*(reward - q)
+
 # Initialise the environment and the agent
 env = gym.make('CartPole-v0')
 agent = BetterAgent(env.action_space)
 
-# Run 10 episodes
-iht = tiles.IHT(1024)
-normalise = np.array([20,8,30,10]) # adjust observations to be similar sizes
-for i in range(10):
+# Run episodes
+for i in range(1000):
   state = env.reset()
   reward = 1
   done = False
+  action = agent.act(state, reward)
   while True:
-    env.render()
-    state = tiles.tiles(iht, 8, state*normalise)
-    action = agent.act(state, reward)
-    state, reward, done, _ = env.step(action)
+    if i > 900:
+      env.render()
+    new_state, reward, done, _ = env.step(action)
     if done:
+      agent.terminal_update(reward, state, action)
       break
+    new_action = agent.act(new_state, reward)
+    agent.update_weights(reward, state, new_state, action, new_action)
+    action = new_action
+    state = new_state
 
 # End the session
 env.close()
